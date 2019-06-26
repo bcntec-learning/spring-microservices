45d3eee2dac32b8945d8e25bd5b861e9e8fc586b
ZFPH0000 <fphilip@houseware.es>
2019-06-26 20:01:58 +0200

    o

--- log-monitor/log-monitor-config-server/src/main/java/bcntec/training/springboot/microservices/logmonitor/configserver/Application.java
+++ log-monitor/log-monitor-config-server/src/main/java/bcntec/training/springboot/microservices/logmonitor/configserver/Application.java
@@ -1,13 +1,21 @@
 package bcntec.training.springboot.microservices.logmonitor.configserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableConfigServer
 public class Application {
+    @Value("${config.name}")
+    String name = "World";
 
+    @RequestMapping("/")
+    public String home() {
+        return "Configuration Server Name :" + name;
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
